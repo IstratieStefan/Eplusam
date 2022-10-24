181392f6c5e17a3937a76eb297e1869b35eac086
Karo8870 <cazacuchristian@gmail.com>
2022-10-24 19:08:49 +0300

    Uf

---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    public void Kill()
+    {
+        //Application.Quit();
+        Debug.Log("Ana are mere mari");
+    }
+}
